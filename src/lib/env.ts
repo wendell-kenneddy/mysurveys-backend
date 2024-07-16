d61be3ad7897c145d8ddc9a15c6519ed65e2b252
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-16 20:49:06 -0300

    feat(env.ts): validate remaining environment variables

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,6 +5,10 @@ const envSchema = z.object({
   API_BASE_URL: z.string().url(),
   ORIGIN: z.string(),
   PORT: z.coerce.number(),
+  ACCESS_TOKEN_SECRET: z.string(),
+  REFRESH_TOKEN_SECRET: z.string(),
+  COOKIE_SECRET: z.string(),
+  NODE_ENV: z.string()
 });
 
 const env = envSchema.parse(process.env);
