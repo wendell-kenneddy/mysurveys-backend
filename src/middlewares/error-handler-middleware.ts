fd19f34c49d521589124b94bc011f1db85bad485
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-16 20:50:29 -0300

    feat(middlewares): add error-handler-middleware

---
+++ src/middlewares/error-handler-middleware.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
+import { AuthenticationError } from "../errors/authentication-error";
+import { AuthorizationError } from "../errors/authorization-error";
+import { ClientError } from "../errors/client-error";
+import { env } from "../lib/env";
+
+export function errorHandlerMiddleware(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (env.NODE_ENV == "development") console.log(err);
+
+  if (err instanceof ZodError) {
+    return res.status(400).json({
+      message: "Invalid input.",
+      errors: err.flatten().fieldErrors
+    });
+  }
+
+  if (err instanceof ClientError) {
+    return res.status(400).json({
+      message: err.message
+    });
+  }
+
+  if (err instanceof AuthenticationError) {
+    return res.status(401).json({
+      message: "Unauthenticated access denied."
+    });
+  }
+
+  if (err instanceof AuthorizationError) {
+    return res.status(403).json({
+      message: "Unauthorized access denied."
+    });
+  }
+
+  return res.status(500).json("Internal server error.");
+}
