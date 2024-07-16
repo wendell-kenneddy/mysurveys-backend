2bfa64ab692c7fc3ad02ff558876d1ef6c3f0a11
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-16 20:52:23 -0300

    feat(auth/services): add signup-service

---
+++ src/modules/auth/services/signup-service.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { hashPassword } from "../../../lib/hashPassword";
+import { prisma } from "../../../lib/prisma";
+
+const newUserSchema = z.object({
+  name: z.string(),
+  email: z.string().email("Invalid e-mail."),
+  password: z.string().min(8, "Password must be at least 8 characters long.")
+});
+
+export class SignupService {
+  async execute(data: unknown) {
+    const newUserData = newUserSchema.parse(data);
+    const hashedPassword = await hashPassword(newUserData.password);
+    const respondantRole = await prisma.role.findFirst({
+      where: { title: "respondant" },
+      select: { id: true }
+    });
+
+    if (!respondantRole) throw new Error("Respondant role not found.");
+
+    const newUser = await prisma.user.create({
+      data: {
+        name: newUserData.name,
+        email: newUserData.email,
+        password: hashedPassword,
+        role_id: respondantRole.id
+      }
+    });
+
+    return newUser.id;
+  }
+}
