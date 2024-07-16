9adc88e9fa2422a1ceda1a806443651b1005a11a
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-16 20:53:50 -0300

    feat(users): add users-router

---
+++ src/modules/users/users-router.ts
@@ -0,0 +1,21 @@
+import Router from "express-promise-router";
+import { UsersController } from "./users-controller";
+import { withAuthMiddleware } from "../../middlewares/with-auth-middleware";
+import { roleAndPermissionsMiddleware } from "../../middlewares/role-and-permissions-middleware";
+
+const usersController = new UsersController();
+const usersRouter = Router();
+
+usersRouter.post(
+  "/users",
+  withAuthMiddleware,
+  roleAndPermissionsMiddleware,
+  usersController.createUser
+);
+usersRouter.get(
+  "/users/profile",
+  withAuthMiddleware,
+  usersController.getProfile
+);
+
+export { usersRouter };
