d17f894f776a5d7458ca27e4c895b1360fc3fa41
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-18 16:58:30 -0300

    feat(surveys): add surveys-router

---
+++ src/modules/surveys/surveys-router.ts
@@ -0,0 +1,45 @@
+import Router from "express-promise-router";
+import { SurveysController } from "./surveys-controller";
+import { withAuthMiddleware } from "../../middlewares/with-auth-middleware";
+import { roleAndPermissionsMiddleware } from "../../middlewares/role-and-permissions-middleware";
+
+const surveysController = new SurveysController();
+const surveysRouter = Router();
+
+surveysRouter.get(
+  "/surveys/:surveyID",
+  withAuthMiddleware,
+  roleAndPermissionsMiddleware,
+  surveysController.getOneSurvey
+);
+surveysRouter.get(
+  "/surveys",
+  withAuthMiddleware,
+  roleAndPermissionsMiddleware,
+  surveysController.getManySurveys
+);
+surveysRouter.post(
+  "/surveys",
+  withAuthMiddleware,
+  roleAndPermissionsMiddleware,
+  surveysController.createSurvey
+);
+surveysRouter.post(
+  "/surveys/:surveyID/answers",
+  withAuthMiddleware,
+  surveysController.answerSurvey
+);
+surveysRouter.delete(
+  "/surveys/:surveyID",
+  withAuthMiddleware,
+  roleAndPermissionsMiddleware,
+  surveysController.deleteSurvey
+);
+surveysRouter.patch(
+  "/surveys/:surveyID",
+  withAuthMiddleware,
+  roleAndPermissionsMiddleware,
+  surveysController.completeSurvey
+);
+
+export { surveysRouter };
