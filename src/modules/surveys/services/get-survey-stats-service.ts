f3719e31ec926c1fd8cdc6bb7802335fa8ec6c1e
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-23 20:10:58 -0300

    feat(surveys/services): add get-survey-stats-service

---
+++ src/modules/surveys/services/get-survey-stats-service.ts
@@ -0,0 +1,47 @@
+import { ClientError } from "../../../errors/client-error";
+import { prisma } from "../../../lib/prisma";
+import { uuidSchema } from "../../../lib/uuid-schema";
+
+export class GetSurveyStatsService {
+  async execute(surveyID: string) {
+    uuidSchema.parse(surveyID);
+
+    const survey = await prisma.survey.findUnique({
+      where: { id: surveyID },
+      select: {
+        completed_at: true,
+        author_id: true,
+        answers: true,
+        _count: {
+          select: {
+            questions: true
+          }
+        }
+      }
+    });
+
+    if (!survey) throw new ClientError("Survey not found.");
+    if (!survey.completed_at) throw new ClientError("Survey not completed.");
+    if (!survey.answers.length) throw new ClientError("Survey has no answers.");
+
+    const frequency = survey.answers.reduce<Record<string, number>>(
+      (acc, curr) => {
+        const questionChoiceTotalKey = `${curr.question_title}:${curr.choice_title}`;
+        const questionChoiceTotalValue = acc[questionChoiceTotalKey];
+
+        acc[questionChoiceTotalKey] = questionChoiceTotalValue
+          ? questionChoiceTotalValue + 1
+          : 1;
+
+        return acc;
+      },
+      {}
+    );
+
+    return {
+      frequency,
+      author_id: survey.author_id,
+      respondantsTotal: survey.answers.length / survey._count.questions
+    };
+  }
+}
