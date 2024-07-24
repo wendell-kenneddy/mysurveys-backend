35e2a4f4220c145f801e573cfd1d6863871d9786
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-24 10:02:44 -0300

    refactor(users-router): remove unnecessary role-and-permissions middleware from routes

--- src/modules/users/users-router.ts
+++ src/modules/users/users-router.ts
@@ -1,17 +1,11 @@
 import Router from "express-promise-router";
 import { UsersController } from "./users-controller";
 import { withAuthMiddleware } from "../../middlewares/with-auth-middleware";
-import { roleAndPermissionsMiddleware } from "../../middlewares/role-and-permissions-middleware";
 
 const usersController = new UsersController();
 const usersRouter = Router();
 
-usersRouter.post(
-  "/users",
-  withAuthMiddleware,
-  roleAndPermissionsMiddleware,
-  usersController.createUser
-);
+usersRouter.post("/users", withAuthMiddleware, usersController.createUser);
 usersRouter.get(
   "/users/profile",
   withAuthMiddleware,
@@ -20,7 +14,6 @@ usersRouter.get(
 usersRouter.delete(
   "/users/profile",
   withAuthMiddleware,
-  roleAndPermissionsMiddleware,
   usersController.deleteUser
 );
 
