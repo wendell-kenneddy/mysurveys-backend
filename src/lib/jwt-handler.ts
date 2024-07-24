d0aa43654d8294740b11f124557cb95a35eb7a1f
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-24 09:57:48 -0300

    feat(jwt-handler): send user role and permissions on jwt

--- src/lib/jwt-handler.ts
+++ src/lib/jwt-handler.ts
@@ -1,19 +1,29 @@
 import { sign } from "jsonwebtoken";
 import { env } from "./env";
+import { z } from "zod";
+
+const tokenDataSchema = z.object({
+  id: z.string().uuid(),
+  name: z.string(),
+  role: z.string(),
+  permissions: z.array(z.string())
+});
 
 export class JWTHandler {
-  static generateAccessToken(userID: string) {
-    const token = sign({ userID }, env.ACCESS_TOKEN_SECRET, {
+  static generateAccessToken(data: unknown) {
+    const userData = tokenDataSchema.parse(data);
+    const token = sign({ ...userData }, env.ACCESS_TOKEN_SECRET, {
       subject: "access-token",
       expiresIn: "1h"
     });
     return token;
   }
 
-  static generateRefreshToken(userID: string) {
-    const token = sign({ userID }, env.REFRESH_TOKEN_SECRET, {
+  static generateRefreshToken(data: unknown) {
+    const userData = tokenDataSchema.parse(data);
+    const token = sign({ ...userData }, env.REFRESH_TOKEN_SECRET, {
       subject: "refresh-token",
-      expiresIn: "12h"
+      expiresIn: "6h"
     });
     return token;
   }
